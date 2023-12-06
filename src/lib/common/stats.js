7da89dd5588c22fea832954603108fbbe1f502da
g7eternal <40625769+g7eternal@users.noreply.github.com>
2023-12-07 00:37:56 +0300

    restructure incoming user data

--- src/lib/common/stats.js
+++ src/lib/common/stats.js
@@ -1,5 +1,6 @@
 import { readable } from "svelte/store";
 import { errorState } from "./stores.js";
+import { STOCKS } from "$lib/common/consts.js";
 
 const DATA_SOURCE = "https://okayeg.com/data/usersrecap.json";
 
@@ -14,7 +15,78 @@ export const stats = readable([], async (set) => {
 
     console.info("Okayegbot data loaded. Entries:", json.length);
 
-    set(json);
+    set(
+      json.map((raw) => {
+        // restructure data:
+        if (raw === null) raw = {};
+
+        const userdata = {
+          _id: raw._id,
+          _raw: raw,
+          username: raw.username,
+          egs: raw.egs || 0,
+          eaten: raw.egseaten || 0,
+          commands: {
+            "=eg": raw.eguses || 0,
+            "=eat": raw.egseaten || 0,
+            "=coinflip": raw.coinflips || 0,
+            "=join / lottery": raw.lotteryjoins || 0,
+            "=trivia": raw.trivia || 0,
+            "=duel": raw.duels || 0,
+            "=roulette": raw.roulettes || 0,
+            Other: 0,
+          },
+          egStats: {
+            total: raw.eguses || 0,
+            w: raw.plus || 0,
+            l: raw.negative || 0,
+            d: raw.zero || 0,
+          },
+          egTransfers: {
+            out: raw.egsgiven || 0,
+            in: raw.egsreceived || 0,
+          },
+          trivia: raw.trivia || 0,
+          duel: {
+            total: raw.duels,
+            w: raw.duelswon || 0,
+            l: (raw.duels || 0) - (raw.duelswon || 0),
+            max: raw.biggestduelwin || 0,
+            min: raw.biggestduelloss || 0,
+          },
+          roulette: {
+            total: raw.roulettes,
+            w: raw.rouletteswon || 0,
+            l: (raw.roulettes || 0) - (raw.rouletteswon || 0),
+            max: raw.biggestroulettewin || 0,
+            min: raw.biggestrouletteloss || 0,
+          },
+          coinflip: {
+            total: raw.coinflips,
+            w: raw.coinflipwins || 0,
+            l: (raw.coinflips || 0) - (raw.coinflipwins || 0),
+            max: raw.biggestcoinflipwin || 0,
+            min: raw.biggestcoinfliploss || 0,
+          },
+          stocks: {},
+          subtotal: {
+            commands: raw.uses || 0,
+            stocks: 0,
+          },
+        };
+
+        userdata.commands.Other =
+          userdata.subtotal.commands - Object.values(userdata.commands).reduce((a, v) => a + v, 0);
+
+        for (const s of STOCKS) {
+          const amount = raw[s] || 0;
+          userdata.stocks[s] = amount;
+          userdata.subtotal.stocks += amount;
+        }
+
+        return userdata;
+      })
+    );
   } catch (e) {
     console.error("Data fetch failed", e);
     errorState.set(e.message);
--- src/lib/common/stores.js
+++ src/lib/common/stores.js
@@ -1,7 +1,6 @@
 import { browser } from "$app/environment";
 import { writable } from "svelte/store";
 import { stats } from "./stats.js";
-import { STOCKS } from "$lib/common/consts.js";
 
 export const errorState = writable("");
 
@@ -24,74 +23,7 @@ stats.subscribe((data) => {
 username.subscribe((newUsername) => {
   const lcase = newUsername.toLowerCase();
 
-  const raw = dataRef.find((user) => user.username === lcase) || null;
-  let userdata = null;
-
-  if (raw !== null) {
-    userdata = {
-      _id: raw._id,
-      _raw: raw,
-      username: raw.username,
-      egs: raw.egs || 0,
-      eaten: raw.egseaten || 0,
-      commands: {
-        "=eg": raw.eguses || 0,
-        "=eat": raw.egseaten || 0,
-        "=coinflip": raw.coinflips || 0,
-        "=join / lottery": raw.lotteryjoins || 0,
-        "=trivia": raw.trivia || 0,
-        "=duel": raw.duels || 0,
-        "=roulette": raw.roulettes || 0,
-        Other: 0,
-      },
-      egStats: {
-        total: raw.eguses || 0,
-        w: raw.plus || 0,
-        l: raw.negative || 0,
-        d: raw.zero || 0,
-      },
-      egTransfers: {
-        out: raw.egsgiven || 0,
-        in: raw.egsreceived || 0,
-      },
-      trivia: raw.trivia || 0,
-      duel: {
-        total: raw.duels,
-        w: raw.duelswon || 0,
-        l: (raw.duels || 0) - (raw.duelswon || 0),
-        max: raw.biggestduelwin || 0,
-        min: raw.biggestduelloss || 0,
-      },
-      roulette: {
-        total: raw.roulettes,
-        w: raw.rouletteswon || 0,
-        l: (raw.roulettes || 0) - (raw.rouletteswon || 0),
-        max: raw.biggestroulettewin || 0,
-        min: raw.biggestrouletteloss || 0,
-      },
-      coinflip: {
-        total: raw.coinflips,
-        w: raw.coinflipwins || 0,
-        l: (raw.coinflips || 0) - (raw.coinflipwins || 0),
-        max: raw.biggestcoinflipwin || 0,
-        min: raw.biggestcoinfliploss || 0,
-      },
-      stocks: {},
-      subtotal: {
-        commands: raw.uses || 0,
-        stocks: 0,
-      },
-    };
-
-    userdata.commands.Other = userdata.subtotal.commands - Object.values(userdata.commands).reduce((a, v) => a + v, 0);
-
-    for (const s of STOCKS) {
-      const amount = raw[s] || 0;
-      userdata.stocks[s] = amount;
-      userdata.subtotal.stocks += amount;
-    }
-  }
-
+  const userdata = dataRef.find((user) => user.username === lcase) || null;
   console.log("Current user:", userdata);
   currentUser.set(userdata);
 
